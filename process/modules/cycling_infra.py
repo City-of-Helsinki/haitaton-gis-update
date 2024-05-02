b8414dd256e6e2f8462b4033f7f9c29ea64c7a69
petri-timlin <petri.timlin@ubigu.fi>
2024-05-02 14:52:48 +0300

    HAI-2342/cycle-infra * Added cycle_infra to automation * Added own buffer setting for cycle_infra ylre_street_classses * Changed db connection to use also env variables * Added field "kadun_nimi" for ylre_katualueet * Added field "kadun_nimi" for cycle_infra data. This data is from ylre_katualueet. * Added geometry validation for cycle_infra data * Cycle_infra data is consist of both by hierarchy and by paatyyppi/alatyyppi

--- process/modules/cycling_infra.py
+++ process/modules/cycling_infra.py
@@ -2,9 +2,16 @@ import geopandas as gpd
 import pandas as pd
 import numpy as np
 from sqlalchemy import create_engine, text
+#from shapely.validation import explain_validity
+from os import path
+from shapely.validation import make_valid
+from shapely.geometry import MultiPolygon, Polygon, GeometryCollection
 
 from modules.config import Config
 from modules.gis_processing import GisProcessor
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 class CycleInfra(GisProcessor):
@@ -19,6 +26,17 @@ class CycleInfra(GisProcessor):
 
         file_name = cfg.local_file(self._module)
         self._store_original_data = cfg.store_orinal_data(self._module)
+        self._ylre_street_class_buffer = cfg.ylre_street_class_buffer(self._module)
+
+        # check that ylre_katualueet file is available
+        if not path.exists(self._cfg.target_buffer_file("ylre_katualueet")):
+            raise FileNotFoundError("ylre katualueet polygon not found")
+
+        # Loading ylre_katualueet dataset
+        ylre_katualueet_filename = cfg.target_buffer_file("ylre_katualueet")
+        self._ylre_katualueet = gpd.read_file(ylre_katualueet_filename)
+        self._ylre_katualueet["geometry"] = self._ylre_katualueet.buffer(self._ylre_street_class_buffer)
+        self._ylre_katualueet_sindex = self._ylre_katualueet.sindex
 
         # Following hierarchy levels included into data
         self._hierarkia = [
@@ -26,27 +44,6 @@ class CycleInfra(GisProcessor):
             "Muu pyöräreitti",
             "Baana",
             "Pääpyöräreitti",
-        ]
-
-        # Following alatyyppi values included into data
-        self._alatyyppi = [
-            "Yhdistetty jalkakäytävä ja pyörätie",
-            #"Jalkakäytävä",
-            "Pyöräkatu",
-            "Pyörätie",
-            "Välikaistalla eroteltu pyörätie",
-            #"Muu polku",
-            "Pyöräkaista",
-            #"Ulkoilureitti",
-            "Tasoeroteltu pyörätie",
-            "Pyöräliikenteen ylityspaikka",
-            #"Kulkuväylä aukiolla",
-            "Jalkakäytävä ja pyörätie samassa tasossa",
-            "Jalkakäytävän tasossa oleva pyörätie",
-            "Puistotie- tai väylä",
-            "Piha- ja/tai kävelykatu",
-            "Välikaistalla erotellut jalkakäytävä ja pyörätie",
-            #"Suojatie",
         ]
 
         # Following main and sub type combinations can be removed from data
@@ -132,6 +129,7 @@ class CycleInfra(GisProcessor):
             "id",
             "uuid",
             "hierarkia_yksisuuntaisuus",
+            "index_right0",
         ]
 
         self._lines = gpd.read_file(file_name)
@@ -180,21 +178,39 @@ class CycleInfra(GisProcessor):
 
         return retval
 
+    def _check_and_set_ylre_classes_id(self, lines: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+        attrs = ["ylre_class", "kadun_nimi"]
+        ylre_katualueet_dissolved = self._ylre_katualueet.dissolve(by=attrs)
+        joined_result = gpd.sjoin(lines, ylre_katualueet_dissolved, predicate='within')
+
+        retval = lines.merge(joined_result[['uuid', 'index_right0', 'index_right1']], how='left', left_on='uuid', right_on='uuid')
+
+        return retval
+
+    def _makeValid(self, data, geom_column):
+        data[geom_column] = data.apply(lambda row: make_valid(row[geom_column]) if not row[geom_column].is_valid else row[geom_column], axis=1)
+        return(data)
+
     def process(self):
         self._process_result_lines = self._lines
 
         # Drop unnecessary data rows base on main and sub type
-        temp = self._drop_not_used_classes_base_on_main_and_sub_types(
+        cycle_lines = self._drop_not_used_classes_base_on_main_and_sub_types(
             self._droppable_types, self._process_result_lines
         )
 
         # Keep rows base on hierarchy list
-        #print(self._process_result_lines)
-        self._process_result_lines = self._keep_rows_base_on_hierarchy_list(
+        hierarchy_lines = self._keep_rows_base_on_hierarchy_list(
             self._hierarkia, self._process_result_lines
         )
-        #print(self._process_result_lines)
-        self._process_result_lines = pd.concat([self._process_result_lines,temp])
+        # Merge hierarchy and cycle lines
+        self._process_result_lines = pd.concat([hierarchy_lines, cycle_lines])
+
+        # Drop dublicates
+        self._process_result_lines.drop_duplicates(subset=["gml_id"], inplace=True)
+
+        # Mark objects which are within YLRE katualueet areas
+        self._process_result_lines = self._check_and_set_ylre_classes_id(self._process_result_lines)
 
         # Buffering configuration
         buffers = self._cfg.buffer(self._module)
@@ -205,22 +221,26 @@ class CycleInfra(GisProcessor):
         target_infra_polys = self._process_result_lines.copy()
         target_infra_polys = self._buffering(target_infra_polys)
 
+        target_infra_polys.rename(columns={"index_right1": "kadun_nimi"}, inplace=True)
         # Drop unnecessary columns
         target_infra_polys = self._drop_unnecessary_columns(
             self._dropped_columns, target_infra_polys
         )
 
         # Dissolve geometries base on attributes listed
-        attrs = ["paatyyppi", "alatyyppi", "silta_alikulku", "hierarkia", "yksisuuntaisuus", ]
+        attrs = ["paatyyppi", "alatyyppi", "silta_alikulku", "hierarkia", "yksisuuntaisuus", "kadun_nimi", ]
         for attr in attrs:
-            target_infra_polys[attr] = target_infra_polys[attr].fillna("")
+            target_infra_polys[attr] = target_infra_polys[attr].fillna("NULL")
 
         target_infra_polys = target_infra_polys.dissolve(by=attrs, aggfunc="sum", as_index=False)
 
         # Explode multipolygon to polygons
         target_infra_polys = target_infra_polys.explode(ignore_index=True)
 
-        #target_infra_polys["geometry"] = target_infra_polys.make_valid()
+        # Validate geometry
+        target_infra_polys = self._makeValid(target_infra_polys, "geometry")
+
+        target_infra_polys = target_infra_polys[~target_infra_polys.is_empty]
         # save to instance
         self._process_result_polygons = target_infra_polys
 
@@ -228,11 +248,9 @@ class CycleInfra(GisProcessor):
         connection = create_engine(self._cfg.pg_conn_uri(), future=True)
 
         if self._store_original_data is not False:
-            self._process_result_polygons.rename_geometry('geom', inplace=True)
-#            self._orig.rename_geometry('geom', inplace=True)
+            self._orig.rename_geometry('geom', inplace=True)
             # persist original data
-#            self._orig.to_postgis(
-            self._process_result_polygons.to_postgis(
+            self._orig.to_postgis(
                 self._store_original_data,
                 connection,
                 "public",
